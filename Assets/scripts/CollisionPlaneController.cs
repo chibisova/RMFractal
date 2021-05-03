358feea49f0adbcbcc36fae852a096b0931446c6
chibisova <chibisova12@gmail.com>
2021-05-04 00:12:00 +0300

    UI

--- Assets/scripts/CollisionPlaneController.cs
+++ Assets/scripts/CollisionPlaneController.cs
@@ -15,7 +15,7 @@ public class CollisionPlaneController : MonoBehaviour
         player = GameObject.FindGameObjectWithTag("Player").transform;
         transform.LookAt(2 * transform.position - player.position); // rotates the plane towards the player
         timeAlive = 0;
-    }
+    } 
 
     // Update is called once per frame
     void Update()
